ef6058e2d95935a90394cf502eaffc01638d9a93
Ved Prakash <91253410+vp171212@users.noreply.github.com>
2023-10-05 22:33:04 +0530

    Add files via upload

---
+++ Model/Remote.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DogDoorApp.Model
+{
+    internal class Remote
+    {
+        private DogDoor _door;
+        private Timer _timer = new Timer(5000);
+
+        public Remote(DogDoor door)
+        {
+            _door = door;
+            _timer.Elapsed += new ElapsedEventHandler(TimerElapsedCloseDoor);
+        }
+
+        public void PressButton()
+        {
+            Console.WriteLine("Pressing the remote control button ...");
+            ToggleDoorState();
+            AutoCloseDoor();
+        }
+
+        private void ToggleDoorState()
+        {
+            if (_door.isOpen)
+            {
+                _door.close();
+            }
+            else
+            {
+                _door.open();
+            }
+        }
+
+        private void AutoCloseDoor()
+        {
+            _timer.Enabled = true;
+        }
+
+        private void TimerElapsedCloseDoor(object sender, ElapsedEventArgs e)
+        {
+            _door.close();
+            _timer.Enabled = false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using DogDoorApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogDoorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DogDoorSimulator.DogDoorTestPath(2000);
+
+            DogDoorSimulator.DogDoorTestPath(10000);
+        }
+    }
+}
